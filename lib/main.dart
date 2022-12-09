2f9feec3e30816792dee4725de479f542573e124
山本　堅心 <s2032149nb@s.chibakoudai.jp>
2022-12-09 17:25:10 +0900

    Update: デバイスに応じたフレキシブル、アダプティブなように実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import './widgets/new_transaction.dart';
@@ -5,7 +8,14 @@ import './widgets/transaction_list.dart';
 import './models/transaction.dart';
 import './widgets/chart.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown,
+  // ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -37,6 +47,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = []; // 取引記録を全て格納
+  bool _showChart = false;
 
   // 過去1週間の取引を格納したList
   List<Transaction> get _recentTransactions {
@@ -77,6 +88,58 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  void _showDialog(String id) {
+    Platform.isIOS
+        ? showCupertinoModalPopup(
+            context: context,
+            builder: (context) => CupertinoAlertDialog(
+              title: Text('iOSHello'),
+              content: Text('iOSContents'),
+              actions: [
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('No'),
+                ),
+                CupertinoDialogAction(
+                  isDestructiveAction: true,
+                  onPressed: () {
+                    Navigator.pop(context);
+                    _deleteTransaction(id);
+                  },
+                  child: Text('Yes'),
+                ),
+              ],
+            ),
+          )
+        : showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('AndroidHello'),
+                content: Text('AndroidContents'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      _deleteTransaction(id);
+                    },
+                    child: Text('OK'),
+                  ),
+                ],
+              );
+            },
+          );
+  }
+
   void _deleteTransaction(String id) {
     setState(() {
       _transactions.removeWhere((tx) => tx.id == id);
@@ -85,42 +148,138 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Personal Expenses',
-          style: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _startAddNewTransaction(context),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
+    final MediaQueryData mediaQuery = MediaQuery.of(context);
+    final bool isLandScape = mediaQuery.orientation == Orientation.landscape;
+
+    final PreferredSizeWidget appBar = Platform.isIOS
+        ? CupertinoNavigationBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            middle: Text(
+              // middleがtitleの代わり
+              'Personal Expenses',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'OpenSans',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  child: Icon(
+                    CupertinoIcons.add,
+                    color: Colors.white,
+                  ),
+                  onTap: () => _startAddNewTransaction(context),
+                )
+              ],
+            ),
+          )
+        : AppBar(
+            title: Text(
+              'Personal Expenses',
+              style: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  Icons.add,
+                  size: 35,
+                ),
+                onPressed: () => _startAddNewTransaction(context),
+              ),
+            ],
+          );
+
+    final Container txListWidget = Container(
+        // transactionListに画面全体の70%分の高さを割り当てる
+        height: (mediaQuery.size.height -
+                appBar.preferredSize.height -
+                mediaQuery.padding.top) *
+            0.7,
+        child: TransactionList(_transactions, _showDialog));
+
+    final pageBody = SafeArea(
+      child: SingleChildScrollView(
         child: Column(
           // ColumnのmainAxisAlignmentで位置を調整可能
-          mainAxisAlignment: MainAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            // Chartを格納しているContainer
-            Chart(_recentTransactions),
-            TransactionList(_transactions, _deleteTransaction),
+            if (isLandScape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Show Chart!',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Switch.adaptive(
+                    activeColor: Theme.of(context).primaryColor,
+                    value: _showChart,
+                    onChanged: (value) {
+                      setState(() {
+                        _showChart = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            if (!isLandScape)
+              Container(
+                  // chartに画面全体の40%分の高さを割り当てる
+                  // mediaQuery.size.height: デバイスの高さ
+                  // appBar.prefferredSize.height: appBarの高さ
+                  // mediaQuery.padding.top: 1番上のステータスバーの高さ
+                  height: (mediaQuery.size.height -
+                          appBar.preferredSize.height -
+                          mediaQuery.padding.top) *
+                      0.3,
+                  child: Chart(_recentTransactions)),
+            if (!isLandScape) txListWidget,
+            if (isLandScape)
+              _showChart
+                  ?
+                  // Chartを格納しているContainer
+                  Container(
+                      // chartに画面全体の40%分の高さを割り当てる
+                      // mediaQuery.size.height: デバイスの高さ
+                      // appBar.prefferredSize.height: appBarの高さ
+                      // mediaQuery.padding.top: 1番上のステータスバーの高さ
+                      height: (mediaQuery.size.height -
+                              appBar.preferredSize.height -
+                              mediaQuery.padding.top) *
+                          0.65,
+                      child: Chart(_recentTransactions))
+                  : txListWidget
           ],
         ),
       ),
-      // floatingButtonの位置決定
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      // floatingButtonの生成
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => _startAddNewTransaction(context),
-      ),
     );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            navigationBar: appBar,
+            child: pageBody,
+          )
+        : Scaffold(
+            appBar: appBar,
+            body: pageBody,
+            // floatingButtonの位置決定
+            floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+            // floatingButtonの生成
+            floatingActionButton: Platform.isIOS
+                ? Container()
+                : FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () => _startAddNewTransaction(context),
+                  ),
+          );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -5,128 +5,146 @@ import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> userTransactions;
-  final Function deleteTransaction;
+  final Function showDeleteDialog;
 
-  TransactionList(this.userTransactions, this.deleteTransaction);
+  TransactionList(this.userTransactions, this.showDeleteDialog);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 500,
-      child: userTransactions.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  'No transactions added yet!',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                // 空白。ただ間を開けるためのもの
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  // 画像のサイズ設定
-                  height: 200,
-                  // Imageの追加方法。pubspec.yamlへの追記も忘れずに
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover, // デバイスサイズによって動的に
+    return userTransactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: [
+                  Text(
+                    'No transactions added yet!',
+                    style: Theme.of(context).textTheme.headline6,
                   ),
-                ),
-              ],
-            )
-          // パフォーマンスを意識したScrollの生成
-          : ListView.builder(
-              // ListViewを使用する際はListの要素数を記述すること
-              itemCount: userTransactions.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 6),
-                  elevation: 6,
-                  child: ListTile(
-                    // leadingは、ListTileの先頭の要素
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text(
-                            // '¥${userTransactions[index].amount.toStringAsFixed(0)}'),
-                            '¥${NumberFormat('#,##0').format(userTransactions[index].amount)}',
+                  // 空白。ただ間を開けるためのもの
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    // 画像のサイズ設定
+                    height: constraints.maxHeight * 0.6,
+                    // Imageの追加方法。pubspec.yamlへの追記も忘れずに
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover, // デバイスサイズによって動的に
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        // パフォーマンスを意識したScrollの生成
+        : ListView.builder(
+            // ListViewを使用する際はListの要素数を記述すること
+            itemCount: userTransactions.length,
+            itemBuilder: (context, index) {
+              return Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 6),
+                elevation: 6,
+                child: ListTile(
+                  // leadingは、ListTileの先頭の要素
+                  leading: CircleAvatar(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text(
+                          // '¥${userTransactions[index].amount.toStringAsFixed(0)}'),
+                          '¥${NumberFormat('#,##0').format(userTransactions[index].amount)}',
+                          style: TextStyle(
+                            color: Colors.white,
                           ),
                         ),
                       ),
                     ),
-                    title: Text(
-                      userTransactions[index].title,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(userTransactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () =>
-                          deleteTransaction(userTransactions[index].id),
-                    ),
                   ),
-                );
-                // return Card(
-                //   child: Row(children: [
-                //     // Containerでないと設定できない項目も多々ある(decoration, margin, padding等)
-                //     Flexible(
-                //       child: Container(
-                //         // decorationでBorderを設定
-                //         decoration: BoxDecoration(
-                //           border: Border.all(
-                //             color: Theme.of(context).primaryColor,
-                //             width: 2,
-                //           ),
-                //         ),
-                //         // symmetricで上下左右へmarginを設定可能
-                //         margin: EdgeInsets.symmetric(
-                //           vertical: 10,
-                //           horizontal: 15,
-                //         ),
-                //         // paddingの設定も可能
-                //         padding: EdgeInsets.all(10),
-                //         child: FittedBox(
-                //           child: Text(
-                //             '\$${userTransactions[index].amount.toStringAsFixed(2)}',
-                //             style: TextStyle(
-                //               fontWeight: FontWeight.bold,
-                //               fontSize: 20,
-                //               color: Theme.of(context).primaryColor,
-                //             ),
-                //           ),
-                //         ),
-                //       ),
-                //     ),
-                //     Column(
-                //       // 子要素を左寄せ
-                //       crossAxisAlignment: CrossAxisAlignment.start,
-                //       children: [
-                //         Text(
-                //           userTransactions[index].title,
-                //           style: Theme.of(context).textTheme.headline6,
-                //         ),
-                //         Text(
-                //           // DateFormatで日付を見やすく(package:intl/intl.dart)をimport忘れずに
-                //           DateFormat.yMMMMd()
-                //               .format(userTransactions[index].date),
-                //           style: TextStyle(
-                //             color: Colors.grey,
-                //             fontWeight: FontWeight.bold,
-                //             fontSize: 16,
-                //           ),
-                //         ),
-                //       ],
-                //     )
-                //   ]),
-                // );
-              },
-            ),
-    );
+                  title: Text(
+                    userTransactions[index].title,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(userTransactions[index].date),
+                  ),
+                  // デバイスの横幅が360以上だったらラベル付きのボタンを生成する
+                  trailing: MediaQuery.of(context).size.width > 360
+                      ? TextButton.icon(
+                          icon: Icon(
+                            Icons.delete,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          label: Text(
+                            'Delete',
+                          ),
+                          onPressed: () =>
+                              showDeleteDialog(userTransactions[index].id),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).primaryColor,
+                          onPressed: () =>
+                              showDeleteDialog(userTransactions[index].id),
+                        ),
+                ),
+              );
+              // return Card(
+              //   child: Row(children: [
+              //     // Containerでないと設定できない項目も多々ある(decoration, margin, padding等)
+              //     Flexible(
+              //       child: Container(
+              //         // decorationでBorderを設定
+              //         decoration: BoxDecoration(
+              //           border: Border.all(
+              //             color: Theme.of(context).primaryColor,
+              //             width: 2,
+              //           ),
+              //         ),
+              //         // symmetricで上下左右へmarginを設定可能
+              //         margin: EdgeInsets.symmetric(
+              //           vertical: 10,
+              //           horizontal: 15,
+              //         ),
+              //         // paddingの設定も可能
+              //         padding: EdgeInsets.all(10),
+              //         child: FittedBox(
+              //           child: Text(
+              //             '\$${userTransactions[index].amount.toStringAsFixed(2)}',
+              //             style: TextStyle(
+              //               fontWeight: FontWeight.bold,
+              //               fontSize: 20,
+              //               color: Theme.of(context).primaryColor,
+              //             ),
+              //           ),
+              //         ),
+              //       ),
+              //     ),
+              //     Column(
+              //       // 子要素を左寄せ
+              //       crossAxisAlignment: CrossAxisAlignment.start,
+              //       children: [
+              //         Text(
+              //           userTransactions[index].title,
+              //           style: Theme.of(context).textTheme.headline6,
+              //         ),
+              //         Text(
+              //           // DateFormatで日付を見やすく(package:intl/intl.dart)をimport忘れずに
+              //           DateFormat.yMMMMd()
+              //               .format(userTransactions[index].date),
+              //           style: TextStyle(
+              //             color: Colors.grey,
+              //             fontWeight: FontWeight.bold,
+              //             fontSize: 16,
+              //           ),
+              //         ),
+              //       ],
+              //     )
+              //   ]),
+              // );
+            },
+          );
   }
 }
